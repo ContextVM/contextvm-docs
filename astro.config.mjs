945a72ca570aa9780570467be9605835d64a83f6
gzuuus <gzuuus@protonmail.com>
2025-07-18 00:08:53 +0200

    fix: base url 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://contextvm.github.io/',
-	base: '/contextvm-docs/',
+	base: 'contextvm-docs',
 	integrations: [
 		starlight({
 			title: 'ContextVM Documentation',
